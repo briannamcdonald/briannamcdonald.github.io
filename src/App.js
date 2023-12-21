5f0bab7a307111705760fb91f8058725adf8e481
Brianna McDonald <brmcdonald@mun.ca>
2023-12-21 14:45:36 -0330

    initial commit to create react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Portfolio website at: <a href="https://briannamcdonald.me/portfolio-website">briannamcdonald.me/portfolio-website</a></p>
+    </div>
+  );
+}
+
+export default App;
